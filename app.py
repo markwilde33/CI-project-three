9238ebdcc1c71ce715152f26d871f90540fa0367
markwilde33 <markwilde29@gmail.com>
2018-12-19 16:25:19 +0000

    Added a function to write to the USER_DATABASE for each instance of a new user. Added a function to write to the LEADERBOARD_DATABASE for each instance of a riddle game. Added a function to read from the ANIMAL_RIDDLE_DATABASE for each instance of a riddle game

--- app.py
+++ app.py
@@ -12,6 +12,29 @@ ANIMAL_RIDDLE_DATABASE = os.path.join(os.path.dirname(
     os.path.abspath(__file__)) + '/data/animal_riddles.json')
 
 
+# ///Riddles list///
+animal_riddles = []
+
+# ///write to user and leaderboard databases///
+
+
+def initialize():
+    # ///write to the user and database///
+    if not os.path.exists(USER_DATABASE):
+        data = {'users': []}
+        with open(USER_DATABASE, 'w') as writer:
+            json.dump(data, writer)
+    # ///write to the leaderboard database///
+    if not os.path.exists(LEADERBOARD_DATABASE):
+        data = {'leaders': []}
+        with open(LEADERBOARD_DATABASE, 'w') as writer:
+            json.dump(data, writer)
+    # ///Read from the riddle database///
+    global animal_riddles
+    with open(ANIMAL_RIDDLE_DATABASE) as reader:
+        animal_riddles = json.load(reader)
+
+
 # ///route to index page///
 @app.route('/')
 @app.route('/index')
@@ -20,6 +43,7 @@ def index():
 
 
 if __name__ == '__main__':
+    initialize()
     app.secret_key = 'secret'
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
