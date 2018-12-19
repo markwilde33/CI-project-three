e91c9e86f1ba3b01da80ceee1ba501c64a3cd94b
markwilde33 <markwilde29@gmail.com>
2018-12-19 15:36:25 +0000

    Set database variables using os.path

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+# ///Set Database variables///
+USER_DATABASE = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)) + '/data/users.json')
+LEADERBOARD_DATABASE = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)) + '/data/leaderboard.json')
+ANIMAL_RIDDLE_DATABASE = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)) + '/data/animal_riddles.json')
 
-# route to index page
 
-
+# ///route to index page///
 @app.route('/')
 @app.route('/index')
 def index():
